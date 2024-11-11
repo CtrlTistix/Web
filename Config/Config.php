222b4a7569778b39a535af750553f94d95ef6929
CtrlTistix <77314210+CtrlTistix@users.noreply.github.com>
2024-11-11 11:26:52 -0500

    si

---
+++ Config/Config.php
@@ -0,0 +1,16 @@
+<?php
+const BASE_URL = "http://localhost/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "ecommerce";
+const CHARSET = "charset=utf8";
+const TITLE = "ClickCart";
+const MONEDA = "USD";
+const CLIENT_ID = "";
+
+const USER_SMTP = "sai020725de@gmail.com";
+const PASS_SMTP = "ozdq yeto sdbg wduy";
+const PUERTO_SMTP = 465;
+const HOST_SMTP = "smtp.gmail.com";
+?>
---
+++ conexion_db.php
@@ -0,0 +1,4 @@
+<?php
+    $conexion=new mysqli("localhost","root","","Login","3306");
+    $conexion->set_charset("utf8");
+?>
